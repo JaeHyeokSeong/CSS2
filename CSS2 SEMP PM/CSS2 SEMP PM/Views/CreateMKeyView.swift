48df4c07dc2921a93e422db0fc3d92c31aa40827
LoadingJohn <johnduw@gmail.com>
2024-03-26 21:28:37 +1100

    Mkey to main page

--- CSS2 SEMP PM/CSS2 SEMP PM/Views/CreateMKeyView.swift
+++ CSS2 SEMP PM/CSS2 SEMP PM/Views/CreateMKeyView.swift
@@ -16,40 +16,44 @@ struct CreateMKeyView: View {
         if showMainPage {
             MainPage()
         }
-        VStack {
-            Image(systemName: "hare.fill")
-                .font(.system(size: 150))
-                .foregroundColor(.black.opacity(0.7))
-            Text("SEMP")
-                .font(.title)
-                .bold()
-                .padding()
-            Text("Please create a Master Password")
-                .font(.title3)
-                .padding()
-            SecureField("Password", text: $password)
-                .padding()
-                .frame(width: 300, height: 50)
-                .background(Color.black.opacity(0.1))
-                .cornerRadius(15)
-                .border(.red.opacity(0.4), width: CGFloat(errorInput))
-            SecureField("Confirm Password", text: $confirmPassword)
-                .padding()
-                .frame(width: 300, height: 50)
-                .background(Color.black.opacity(0.1))
-                .cornerRadius(15)
-                .border(.red.opacity(0.4), width: CGFloat(errorInput))
-            Button("Login") {
-                matchPasswords()
+        NavigationView {
+            VStack {
+                Image(systemName: "hare.fill")
+                    .font(.system(size: 150))
+                    .foregroundColor(.black.opacity(0.7))
+                Text("SEMP")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                Text("Please create a Master Password")
+                    .font(.title3)
+                    .padding()
+                SecureField("Password", text: $password)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(15)
+                    .border(.red.opacity(0.4), width: CGFloat(errorInput))
+                SecureField("Confirm Password", text: $confirmPassword)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(15)
+                    .border(.red.opacity(0.4), width: CGFloat(errorInput))
+                Button("Login") {
+                    matchPasswords()
+                }
+                .font(.system(size: 28))
+                .foregroundColor(.white.opacity(0.9))
+                .frame(width: 100, height: 30)
+                .background(Color.black.opacity(0.9))
+                .cornerRadius(10)
+                NavigationLink(destination: MainPage(), isActive: $showMainPage) {
+                }
             }
-            .font(.system(size: 28))
-            .foregroundColor(.white.opacity(0.9))
-            .frame(width: 100, height: 30)
-            .background(Color.black.opacity(0.9))
-            .cornerRadius(10)
         }
+        .navigationBarHidden(true)
     }
-    
     func matchPasswords() {
         if password == "" || confirmPassword == "" {
             errorInput = 3
@@ -62,8 +66,6 @@ struct CreateMKeyView: View {
         }
     }
 }
-
-
 
 #Preview {
     CreateMKeyView()
