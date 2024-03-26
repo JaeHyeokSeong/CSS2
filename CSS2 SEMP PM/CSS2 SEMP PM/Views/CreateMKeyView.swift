84f007a93e42e90dada11a02a587d4a937abcd04
LoadingJohn <johnduw@gmail.com>
2024-03-26 20:43:01 +1100

    Master Key View

--- CSS2 SEMP PM/CSS2 SEMP PM/Views/CreateMKeyView.swift
+++ CSS2 SEMP PM/CSS2 SEMP PM/Views/CreateMKeyView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct CreateMKeyView: View {
+    @State var password = ""
+    @State var confirmPassword = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "hare.fill")
+                .font(.system(size: 150))
+                .foregroundColor(.black.opacity(0.7))
+            Text("SEMP")
+                .font(.title)
+                .bold()
+                .padding()
+            Text("Please create a Master Password")
+                .font(.title3)
+                .padding()
+            SecureField("Password", text: $password)
+                .padding()
+                .frame(width: 300, height: 50)
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(15)
+            SecureField("Confirm Password", text: $confirmPassword)
+                .padding()
+                .frame(width: 300, height: 50)
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(15)
+        }
     }
+}
+
+func matchPasswords() {
+
 }
 
 #Preview {
