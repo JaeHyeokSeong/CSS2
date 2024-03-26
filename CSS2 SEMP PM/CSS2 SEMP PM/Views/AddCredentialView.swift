14eff0e12d178aef7646df8f7f4a0375140c4fe8
aljonn <santosaljonn13@gmail.com>
2024-03-26 21:50:01 +1100

    add cred view

--- CSS2 SEMP PM/CSS2 SEMP PM/Views/AddCredentialView.swift
+++ CSS2 SEMP PM/CSS2 SEMP PM/Views/AddCredentialView.swift
@@ -8,18 +8,46 @@
 import SwiftUI
 
 struct AddCredentialView: View {
+    @State var _title: String = ""
+    @State var _email: String = ""
+    @State var _password: String = ""
+    @State var _siteTitle: String = ""
+    @State var _siteAddress: String? = ""
+//    @State var _healthStatus: Int = ""
+//    @State var _reachedStatus: Int = ""
+//    @State var _timeToChange: Int = ""
+    @State var _notes: String? = ""
+    @State var _encryptionMethod: String = ""
+    
     var body: some View {
         VStack {
             HStack {
                 Image(systemName: "hare.fill")
                 Text("Add Credentials")
             }
-           
             .padding()
+            
             
             HStack {
                 Text("Title:")
-                
+                    .padding(.trailing, 10.0)
+                TextField("Title", text: $_title)
+                    .frame(width:200.0)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            HStack {
+                Text("Email:")
+                    .padding(.trailing, 10.0)
+                TextField("Email", text: $_email)
+                    .frame(width:200.0)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            HStack {
+                Text("Password:")
+                    .padding(.trailing, 10.0)
+                TextField("Password", text: $_password)
+                    .frame(width:200.0)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
             }
             
             
