ee2bfe21918ac577d8a4c50b2a3e8f80687d26c7
LoadingJohn <johnduw@gmail.com>
2024-03-30 19:07:31 +1100

    Please work

---
+++ CSS2 SEMP PM/CSS2 SEMP PM/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  CSS2 SEMP PM
+//
+//  Created by John Underwood on 30/3/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
