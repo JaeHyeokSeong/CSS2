e104be2c5f9c8bf4adece894a201fe86d0deb6ff
LoadingJohn <johnduw@gmail.com>
2024-03-30 19:19:36 +1100

    lgtm

--- CSS2 SEMP PM/CSS2 SEMP PM/Views/AddCredentialView.swift
+++ CSS2 SEMP PM/CSS2 SEMP PM/Views/AddCredentialView.swift
@@ -9,71 +9,66 @@ import SwiftUI
 
 struct AddCredentialView: View {
     @EnvironmentObject var viewModel: ViewModel
-@State var credential: Credentials = Credentials(email: "", password: "", siteTitle: "", siteAddress: nil, healthStatus: 0, breachedStatus: 0, timeToChange: 30, notes: nil, encryptionMethod: "")
-@State var encryptSelection = "Select Encryption Method"
-@State var isComplete = false
+    @State var credential: Credentials = Credentials(email: "", password: "", siteTitle: "", siteAddress: nil, healthStatus: 0, breachedStatus: 0, timeToChange: 30, notes: nil, encryptionMethod: "")
+    @State var encryptSelection = "Select Encryption Method"
+    @State var isComplete = false
     
-var body: some View {
-    NavigationStack {
-                VStack {
-                        HStack {
-                            Image(systemName: "hare.fill")
-                            Text("Add Credentials")
+    var body: some View {
+        NavigationStack {
+            VStack {
+                HStack {
+                    Image(systemName: "hare.fill")
+                    Text("Add Credentials")
+                }
+                .padding()
+                VStack{
+                    Form {
+                        Section(header: Text("Site Information")){
+                            TextField("Title", text: $credential.siteTitle)
+                            TextField("Site Address", text: Binding<String>( get: { credential.siteAddress ?? "" }, set: { credential.siteAddress = $0 }
+                                                                           ))
+                            .keyboardType(.URL)
                         }
-                        .padding()
-                        
-                        VStack{
-                            Form {
-                                Section(header: Text("Site Information")){
-                                    TextField("Title", text: $credential.siteTitle)
-                                    TextField("Site Address", text: Binding<String>(
-                                        get: { credential.siteAddress ?? "" }, set: { credential.siteAddress = $0 }
-                                    ))
-                                        .keyboardType(.URL)
+                        Section(header: Text("Credentials")) {
+                            TextField("Email", text: $credential.email)
+                                .keyboardType(.emailAddress)
+                            SecureField("Password", text: $credential.password)
+                        }
+                        Section(header: Text("Encryption Method")) {
+                            Menu(encryptSelection) {
+                                Button("AES"){
+                                    encryptSelection = "AES"
                                 }
-                                Section(header: Text("Credentials")) {
-                                    TextField("Email", text: $credential.email)
-                                        .keyboardType(.emailAddress)
-                                    SecureField("Password", text: $credential.password)
+                                Button("RSA"){
+                                    encryptSelection = "RSA"
                                 }
-                                Section(header: Text("Encryption Method")) {
-                                    Menu(encryptSelection) {
-                                        Button("AES"){
-                                            encryptSelection = "AES"
-                                        }
-                                        Button("RSA"){
-                                            encryptSelection = "RSA"
-                                        }
-                                        Button("SHA"){
-                                            encryptSelection = "SHA"
-                                        }
-                                    }
-                                }
-                                Section(header: Text("Notes")){
-                                    TextField("Notes", text: Binding<String>(
-                                        get: { credential.notes ?? "" }, set: { credential.notes = $0 }
-                                    ), axis: .vertical)
-                                    .lineLimit(3...)
-                                    
-                                }
-                                Section {
-                                    Button("Add") {
-                                        //credentials.append(credential) //need to check if all fields are filled.
-                                        isComplete = true
-                                    }
-                                    .frame(maxWidth: .infinity, alignment: .center)
-                                    .navigationDestination(isPresented: $isComplete){
-                                        MainPage().environmentObject(viewModel)
-                                            .navigationBarBackButtonHidden()
-                                    }
-                                    
+                                Button("SHA"){
+                                    encryptSelection = "SHA"
                                 }
                             }
+                        }
+                        Section(header: Text("Notes")){
+                            TextField("Notes", text: Binding<String>(
+                                get: { credential.notes ?? "" }, set: { credential.notes = $0 }), axis: .vertical).lineLimit(3...)
+                        }
+                        Section {
+                            Button("Add") {
+                                //credentials.append(credential) //need to check if all fields are filled.
+                                isComplete = true
+                            }
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .navigationDestination(isPresented: $isComplete){
+                                MainPage().environmentObject(viewModel)
+                                    .navigationBarBackButtonHidden()
+                            }
+                            
                         }
                     }
                 }
             }
         }
+    }
+}
 
 #Preview {
     AddCredentialView().environmentObject(ViewModel())
