9e95b32477033fa324e24e5c7cddb9f3160de40c
LoadingJohn <johnduw@gmail.com>
2024-03-30 19:10:58 +1100

    relook

--- CSS2 SEMP PM/CSS2 SEMP PM/Views/MainPage.swift
+++ CSS2 SEMP PM/CSS2 SEMP PM/Views/MainPage.swift
@@ -8,115 +8,66 @@
 import SwiftUI
 
 struct MainPage: View {
-//    @EnvironmentObject var viewModel: ViewModel
-//    @State private var searchText: String = ""
-//    @State private var showingAddCredentialView = false
-//    
-//    var body: some View {
-//        NavigationView {
-//            VStack {
-//                ZStack(alignment: .leading) {
-//                    RoundedRectangle(cornerRadius: 8)
-//                        .stroke(Color.gray, lineWidth: 1)
-//                    
-//                    HStack {
-//                        Image(systemName: "magnifyingglass")
-//                            .foregroundColor(.gray)
-//                            .padding(.leading, 8)
-//                        TextField("Search", text: $searchText)
-//                            .padding(.horizontal, 8)
-//                    }
-//                }
-//                .frame(height: 40)
-//                .frame(width: 360)
-//                .padding(.bottom, 20)
-//                Spacer()
-//                
-//                List($viewModel.filteredCredentials(searchText: searchText), id: \.id) { credential in
-//                    NavigationLink(destination: DetailView(credential: credential)) {
-//                        VStack(alignment: .leading) {
-//                            Text("Email: \(credential.email)")
-//                            Text("Password: \(credential.password)")
-//                            Text("Link: \(credential.siteAddress)")
-//                        }
-//                        .padding(.vertical, 10)
-//                    }
-//                }
-//                
-//            }
-//            .navigationBarTitle("Passwords", displayMode: .inline)
-//            .navigationBarItems(trailing:
-//                Button(action: {
-//                    showingAddCredentialView = true
-//                }) {
-//                    Image(systemName: "plus")
-//                }
-//            )
-//            .sheet(isPresented: $showingAddCredentialView) {
-//                AddCredentialView().environmentObject(viewModel)
-//            }
-//        }
-//    }
     @EnvironmentObject var viewModel: ViewModel
-        @State private var searchText: String = ""
-        @State private var showingAddCredentialView = false
-        @State private var filteredCredentials: [Credentials] = []
+    @State private var searchText: String = ""
+    @State private var showingAddCredentialView = false
+    @State private var filteredCredentials: [Credentials] = []
         
-        var body: some View {
-            NavigationView {
-                VStack {
-                    ZStack(alignment: .leading) {
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.gray, lineWidth: 1)
-                        
-                        HStack {
-                            Image(systemName: "magnifyingglass")
-                                .foregroundColor(.gray)
-                                .padding(.leading, 8)
-                            TextField("Search", text: $searchText)
-                                .padding(.horizontal, 8)
-                        }
-                    }
-                    .frame(height: 40)
-                    .frame(width: 360)
-                    .padding(.bottom, 20)
-                    Spacer()
+    var body: some View {
+        NavigationView {
+            VStack {
+                ZStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.gray, lineWidth: 1)
                     
-                    List(filteredCredentials) { credential in
-                        NavigationLink(destination: DetailView().environmentObject(viewModel)) {
-                            VStack(alignment: .leading) {
-                                Text("Email: \(credential.email)")
-                                Text("Password: \(credential.password)")
-                                Text("Link: \(credential.siteAddress ?? "")")
-                            }
-                            .padding(.vertical, 10)
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .padding(.leading, 8)
+                        TextField("Search", text: $searchText)
+                            .padding(.horizontal, 8)
+                    }
+                }
+                .frame(height: 40)
+                .frame(width: 360)
+                .padding(.bottom, 20)
+                Spacer()
+                
+                List(filteredCredentials) { credential in
+                    NavigationLink(destination: DetailView().environmentObject(viewModel)) {
+                        VStack(alignment: .leading) {
+                            Text("Email: \(credential.email)")
+                            Text("Password: \(credential.password)")
+                            Text("Link: \(credential.siteAddress ?? "")")
                         }
+                        .padding(.vertical, 10)
                     }
-                    
                 }
-                .navigationBarTitle("Passwords", displayMode: .inline)
-                .navigationBarItems(trailing:
-                    Button(action: {
-                        showingAddCredentialView = true
-                    }) {
-                        Image(systemName: "plus")
-                    }
-                )
-                .sheet(isPresented: $showingAddCredentialView) {
-                    AddCredentialView().environmentObject(viewModel)
+                
+            }
+            .navigationBarTitle("Passwords", displayMode: .inline)
+            .navigationBarItems(trailing:
+                Button(action: {
+                    showingAddCredentialView = true
+                }) {
+                    Image(systemName: "plus")
                 }
-                .onAppear {
-                    updateFilteredCredentials()
-                }
-                .onChange(of: searchText) { _ in
-                    updateFilteredCredentials()
-                }
+            )
+            .sheet(isPresented: $showingAddCredentialView) {
+                AddCredentialView().environmentObject(viewModel)
+            }
+            .onAppear {
+                updateFilteredCredentials()
+            }
+            .onChange(of: searchText) { _ in
+                updateFilteredCredentials()
             }
         }
+    }
         
-        func updateFilteredCredentials() {
-            filteredCredentials = viewModel.filteredCredentials(searchText: searchText)
-        }
+    func updateFilteredCredentials() {
+        filteredCredentials = viewModel.filteredCredentials(searchText: searchText)
+    }
 }
 
 #Preview {
